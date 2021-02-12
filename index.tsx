6dbac1e71766f2b72e5ad49678d7b69faad1142e
Marc Dwyer <marcdwyer@marcs-air.attlocal.net>
2021-02-12 01:23:09 -0600

    hardcoded meta tag in html

--- index.tsx
+++ index.tsx
@@ -1,18 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Helmet } from "react-helmet";
 
 import App from "./src/App";
-import { ImageUrls } from "./src/app_data/urls";
+
 import "./index.scss";
-console.log("is this live");
-ReactDOM.render(
-  <>
-    <Helmet>
-      <title>Marc's portfolio</title>
-      <meta property="og:image" content={ImageUrls.myProfile} />
-    </Helmet>
-    <App />
-  </>,
-  document.getElementById("root")
-);
+
+ReactDOM.render(<App />, document.getElementById("root"));
